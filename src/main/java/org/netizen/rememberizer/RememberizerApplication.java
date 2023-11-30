9590b1e862d65451cee5bbb7aa1fe6d6ff1b22a5
Vlas Nagibin <nagibinvlas@gmail.com>
2023-11-30 06:08:37 +0300

    RMR-1: Initial commit

---
+++ src/main/java/org/netizen/rememberizer/RememberizerApplication.java
@@ -0,0 +1,13 @@
+package org.netizen.rememberizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/netizen/rememberizer/RememberizerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.netizen.rememberizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
